a6767a747a13464bcb4f0028d61a27c8d593fb13
Bandiu <Bandiubandi@gmail.com>
2023-12-10 02:34:28 +0300

    start version 2

---
+++ src/main/java/com/bandiu/fileBr/FileBrApplication.java
@@ -0,0 +1,13 @@
+package com.bandiu.fileBr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandiu/fileBr/FileBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandiu.fileBr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
