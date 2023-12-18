c84eabc9dff16f7adc0f703dac8ef16b5b5c14cc
Bandiu <Bandiubandi@gmail.com>
2023-12-18 10:05:11 -0800

    someChanges

---
+++ src/main/java/com/bandiu/fileBr/service/UserService.java
@@ -0,0 +1,35 @@
+package com.bandiu.fileBr.service;
+
+import com.bandiu.fileBr.model.User;
+import com.bandiu.fileBr.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    private UserService() {
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null)
+            throw new UsernameNotFoundException("User not found");
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),user.getPasswordHash(),new ArrayList<>()
+        );
+    }
+}
+
+
