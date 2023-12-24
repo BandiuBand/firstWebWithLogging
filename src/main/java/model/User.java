154cc2b0c8366c32eb388b62272f5131728617fd
Bandiu <Bandiubandi@gmail.com>
2023-12-24 03:02:14 +0300

    add mor Variable

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,21 +1,24 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-@Entity
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+
+import java.util.Date;
+
+
+public class User{
+
+
 
     private String username;
+
+
     private String passwordHash;
 
-    public int getId() {
-        return id;
-    }
+    private String email;
+
+    private Date createDate;
+
+    private String validationToken;
+    private boolean isValidated;
 
     public String getUsername() {
         return username;
@@ -25,15 +28,52 @@ public class User {
         return passwordHash;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
     public void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getValidationToken() {
+        return validationToken;
+    }
+
+    public void setValidationToken(String validationToken) {
+        this.validationToken = validationToken;
+    }
+
+    public boolean isValidated() {
+        return isValidated;
+    }
+
+    public void setValidated(boolean validated) {
+        isValidated = validated;
+    }
+
+    public User(String username, String passwordHash, String email, Date createDate, String validationToken, boolean isValidated) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+        this.createDate = createDate;
+        this.validationToken = validationToken;
+        this.isValidated = isValidated;
     }
 }
